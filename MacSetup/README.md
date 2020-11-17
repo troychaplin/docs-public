0db614aa94f24945e91839e37fc6fc8a6ea13452
Troy Chaplin <troy.chaplin@gmail.com>
2020-11-16 23:22:29 -0500

    Adding base for mac config script

---
+++ MacSetup/README.md
@@ -0,0 +1,12 @@
+# Homebrew Script for OSX
+
+To execute: save and `chmod +x ./macos-fresh-install.sh` then `./macos-fresh-install.sh`
+
+## Backups
+
+Don't be dumb, backup all this shit before you format or you will regret it.
+
+Don't forget to save/export:
+
+- SSH keys
+- SFTP configs
---
+++ MacSetup/macInstall.sh
@@ -0,0 +1,46 @@
+!/bin/sh
+
+# Homebrew Script for OSX
+# To execute: save and `chmod +x ./macos-fresh-install.sh` then `./macos-fresh-install.sh`
+
+echo "Updating gems..."
+gem update
+
+echo "Installing brew..."
+/usr/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Installing brew cask..."
+brew tap homebrew/cask
+
+# Dev Tools
+brew install node
+brew install git
+brew install composer
+brew install gulp-cli
+brew install node-sass
+brew tap mongodb/brew
+brew install mongodb-community
+brew cask install mongodb-compass
+brew cask install visual-studio-code
+brew cask install figma
+brew cask install sequel-pro
+brew cask install cyberduck
+
+# Communication Apps
+# brew cask install skype
+# brew cask install slack
+
+# Web Tools
+brew cask install google-chrome
+brew cask install firefox
+brew cask install postman
+
+# Password Manager
+brew cask install enpass
+
+# Entertainment
+brew cask install spotify
+
+#Productivity 
+# brew cask install keepingyouawake
+# brew cask install rectangle
