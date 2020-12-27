460962d964b6b31fcd16b625247b3be8d352c176
Troy Chaplin <troy.chaplin@gmail.com>
2020-12-26 19:00:28 -0500

    Update macInstall.sh

--- MacSetup/macInstall.sh
+++ MacSetup/macInstall.sh
@@ -10,12 +10,6 @@ brew upgrade
 echo "Insalling Oh My ZSH..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# RVM Manager
-echo "Installing Ruby and avoid editing system verion..."
-brew install gnupg
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
 # Dev Tools
 echo "Installing dev tools..."
 brew install composer
